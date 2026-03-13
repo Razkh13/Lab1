eb6878478ed0661d5a8354e34a807b39f1e254b3
Razkh13 <nikolya_zakharov_87@inbox.ru>
2026-03-13 12:47:25 +0700

    Добавлено ограничение на отрицательные числа

--- prymoygolnik.cpp
+++ prymoygolnik.cpp
@@ -9,11 +9,19 @@ int main()
     cin >> dlina;
     cout << "Введите ширину прямоугольника ";
     cin >> shirina;
+
+      while (dlina < 0 || shirina < 0) {
+        cout << "Результат параметров не может быть отрицательным" << endl;
+        cout << "Попробуйте еще раз ввести числа" << endl;
+        cin >> dlina >> shirina;
+    }
+
     int P = (dlina + shirina) * 2;
     int S = dlina * shirina;
     double D = sqrt(pow(dlina, 2) + pow(shirina, 2));
     cout << "Периметр, площадь и длина диагонали вашего прямоуголька равны: ";
-    cout << P << " " << S << " " << D << endl;
+    cout << P << "(P), " << S << "(S), " << D << "(D)" << endl; cout << P << " " << S << " " << D << endl;
     cout << "Параметры прямоугольника посчитаны!" << endl;
+
     return 0;
 }
